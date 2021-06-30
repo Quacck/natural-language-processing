48bf30435fc6c16dd28032f5993809516b446515
Vincent <vincopi@googlemail.com>
2021-07-01 00:26:25 +0200

    Create word embedding model

--- assignment1/simple_linguistic_model.py
+++ assignment1/simple_linguistic_model.py
@@ -6,45 +6,23 @@ from sklearn import feature_extraction
 from sklearn.feature_extraction import text
 from sklearn.feature_extraction.text import CountVectorizer
 
-numberOfDataPoints = 10000
+numberOfDataPoints = 100000
 
 def createCorpus(data):
     result = data['instructions'].to_numpy().tolist()[1:numberOfDataPoints]
     result = list(filter(lambda entry: isinstance(entry, str), result))
-    return list(map(lambda line: re.findall("[A-Za-z]+", line), result))
-    
-
-def createDictionaries(listOfFeatures):
-    featureToIndex = {}
-    indexToFeature = {}
-
-    index = 0
-
-    for feature in listOfFeatures:
-        featureToIndex[feature] = index
-        indexToFeature[index] = feature
-        index += 1
-    return featureToIndex, indexToFeature
-
-def buildEmbeddingTrainingData(corpus, featureToIndex, indexToFeature, vectorSize):
-    result = []
-    for text in corpus:
-        onlyText = re.findall("[A-Za-z]+", text.lower())
-        
-    return result
+    return list(map(lambda line: re.findall("[A-Za-z]+", line.lower()), result))
 
 def main():
-    # stop_words = text.ENGLISH_STOP_WORDS.union(["<LINEBREAK>", "<LISTELEMENT>"])
-
     data = pandas.read_csv('preprocessed_data.csv', '|')
     corpus = createCorpus(data)
 
+
     model = gensim.models.Word2Vec(corpus)
 
-    model.wv.save_word2vec_format('word2vec.bin')
+    model.wv.save('word2vec.bin')
+    #model = gensim.models.KeyedVectors.load('word2vec.bin')
 
-    print(model.wv['cake'])  # get numpy vector of a word
-
-    print(model.wv.most_similar('cake', topn=10))  # get other similar words
+    #print(model.most_similar('beans', topn=10))  # get other similar words
 if __name__ == "__main__":
     main()
